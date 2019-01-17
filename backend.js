a3c603630c87cbeecc41ba704a17e6b24682d027
Starla <starla@Starlade-MacBook-Pro.local>
2019-01-17 18:47:26 +0800

    fix

--- backend.js
+++ backend.js
@@ -1,26 +1,34 @@
-// function init() {
-//     console.log("here");
-//     loadJSON("https://iot.martinintw.com/api/v1/data/12345614?fbclid=IwAR1avxnTvuTGdlLdTC9B8-1pQhIAgx6T-jEQG_jlypHPAOgmYiUKzmtq2UA", data, 'jsonp');
-//     // $.getJSON('http://anyorigin.com/get?url=http%3A//webapp.armadealo.com/home.json&callback=?', function(data){
-//     //     $('#output').html(data.contents);
-//     // });
+function init() {
+    console.log("here");
+
+    // $.getJSON('http://anyorigin.com/get?url=http%3A//webapp.armadealo.com/home.json&callback=?', function(data){
+    //     $('#output').html(data.contents);
+    // });
+    // fetch("https://iot.martinintw.com/api/v1/data/12345617", {'mode': 'no-cors'}).then(function(response) {
+    //   return response.json();
+    // }).then(function(myJson) {
+    //   console.log(myJson);
+    // });
+    // console.log(data);
+}
+$.ajax({
+    url: 'https://iot.martinintw.com/api/v1/data/12345617',
+    type: 'GET',
+    dataType: "json",
+    mode: 'no-cors'
+});
+
+window.onload = function () {
+    init();
+};
+
+
+// let data = {}; 
+// function setup() {
+//     data = loadJSON("https://iot.martinintw.com/api/v1/data/12345617");
 //     console.log(data);
 // }
-// // $.ajax({
-// //     url: 'https://iot.martinintw.com/api/v1/data/12345617',
-// //     type: 'GET',
-// //     dataType: "json",
-// // });
 
-// window.onload = function () {
-//     init();
-// };
-let data = {}; 
-function setup() {
-    data = loadJSON("https://iot.martinintw.com/api/v1/data/12345617");
-    console.log(data);
-}
-
-function draw() {
-  ellipse(50, 50, 80, 80);
-}
+// function draw() {
+//   ellipse(50, 50, 80, 80);
+// }
