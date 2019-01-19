f5c85aa5abd031e412300f1c362f5cd1b696f50f
clairewang87 <clairewang8702@gmail.com>
2019-01-19 17:27:00 +0800

    增加一小時內使用狀況

--- backend.js
+++ backend.js
@@ -196,18 +196,12 @@ function parseData(data){
     day[i] = created_at.split(" ")[0];
     time[i] = created_at.split(" ")[1];
 	  week[i] = new Date(created_at).getDay();
-<<<<<<< HEAD
 	
-=======
->>>>>>> 8a1a3a29023d48945b89d29a7cce53a06282ffc1
 
   }
   hourStats(data);
   weekStats(data);
-<<<<<<< HEAD
   latestHourStas(data)
-=======
->>>>>>> 8a1a3a29023d48945b89d29a7cce53a06282ffc1
 
   
 }
@@ -253,7 +247,6 @@ let hourChart = document.getElementById('hour').getContext('2d');
 let usagePerDay = document.getElementById('chart').getContext('2d');
 let weekChart = document.getElementById('week').getContext('2d');
 let latestHourChart = document.getElementById('latesthour').getContext('2d');
-
 
 // 定義顏色
 window.chartColors = {
